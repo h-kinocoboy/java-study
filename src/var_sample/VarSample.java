95f5f645e1486b1dc2812025e824824b56915320
h-kinocoboy <h.kinocoboy@gmail.com>
2018-09-12 16:14:52 +0900

    変数定義

---
+++ src/var_sample/VarSample.java
@@ -0,0 +1,12 @@
+package var_sample;
+
+import java.util.ArrayList;
+
+public class VarSample {
+    int number = 12;
+    String str = "hello";
+    int[] list = new int[3];
+    ArrayList<String> arrList = new ArrayList<String>();
+
+    final int NUMBER = 5; // final をつけると定数定義
+}
